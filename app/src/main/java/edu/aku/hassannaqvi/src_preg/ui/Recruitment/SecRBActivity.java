19056c48808a17a6e2a14f0d601e143eff2e02a6
javed.khan <javedak09@gmail.com>
2017-12-06 17:04:00 +0500

    Section RB Validation, Range and Skip

--- app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/Recruitment/SecRBActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/Recruitment/SecRBActivity.java
@@ -3,6 +3,9 @@ package edu.aku.hassannaqvi.src_preg.ui.Recruitment;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -22,6 +25,7 @@ public class SecRBActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sec_rb);
+        setupViews();
         binding.setCallback(this);
     }
 
@@ -114,6 +118,11 @@ public class SecRBActivity extends Activity {
         }
 
 
+        if (!validatorClass.RangeTextBox(this, binding.rb01, 15, 50, getString(R.string.day), "days")) {
+            return false;
+        }
+
+
         if (!validatorClass.EmptyTextBox(this, binding.rb02, getString(R.string.rb02))) {
             return false;
         }
@@ -123,20 +132,22 @@ public class SecRBActivity extends Activity {
             return false;
         }
 
-
-        if (!validatorClass.EmptyTextBox(this, binding.rb03n, getString(R.string.rb03n))) {
-            return false;
+        if (binding.rb03a.isChecked()) {
+            if (!validatorClass.EmptyTextBox(this, binding.rb03n, getString(R.string.rb03n))) {
+                return false;
+            }
         }
 
 
         if (!validatorClass.EmptyRadioButton(this, binding.rb04, binding.rb04b, getString(R.string.rb04))) {
             return false;
-
         }
 
 
-        if (!validatorClass.EmptyTextBox(this, binding.rb04n, getString(R.string.rb04n))) {
-            return false;
+        if (binding.rb04a.isChecked()) {
+            if (!validatorClass.EmptyTextBox(this, binding.rb04n, getString(R.string.rb04n))) {
+                return false;
+            }
         }
 
 
@@ -145,62 +156,85 @@ public class SecRBActivity extends Activity {
         }
 
 
+        if (!validatorClass.RangeTextBox(this, binding.rb05n, 1, 12, getString(R.string.rb05n), "live birth")) {
+            return false;
+        }
+
+
         if (!validatorClass.EmptyRadioButton(this, binding.rb06, binding.rb06b, getString(R.string.rb06))) {
             return false;
         }
 
+        if (binding.rb06a.isChecked()) {
+            if (!binding.rb0799.isChecked() && !validatorClass.EmptyTextBox(this, binding.rb07m, getString(R.string.rb07m))) {
+                return false;
+            }
 
-        if (!binding.rb0799.isChecked() && !validatorClass.EmptyTextBox(this, binding.rb07m, getString(R.string.rb07m))) {
-            return false;
+
+            if (!binding.rb0799.isChecked() && !validatorClass.RangeTextBox(this, binding.rb07m, 0, 9, getString(R.string.rb07m), "months")) {
+                return false;
+            }
+
+
+            if (!binding.rb0899.isChecked() && !validatorClass.EmptyTextBox(this, binding.rb08n, getString(R.string.rb08n))) {
+                return false;
+            }
+
+
+            if (!binding.rb0899.isChecked() && !validatorClass.RangeTextBox(this, binding.rb08n, 1, 9, getString(R.string.rb08n), "number of time")) {
+                return false;
+            }
+
+
+            if (!validatorClass.EmptyRadioButton(this, binding.rb09vd, binding.rb09vdb, getString(R.string.rb09vd))) {
+                return false;
+            }
+
+
+            if (!validatorClass.EmptyRadioButton(this, binding.rb09i, binding.rb09ib, getString(R.string.rb09i))) {
+                return false;
+            }
+
+
+            if (!validatorClass.EmptyRadioButton(this, binding.rb09fa, binding.rb09fab, getString(R.string.rb09fa))) {
+                return false;
+            }
+
+
+            if (!validatorClass.EmptyRadioButton(this, binding.rb09m, binding.rb09mb, getString(R.string.rb09m))) {
+                return false;
+            }
+
+
+            if (!validatorClass.EmptyRadioButton(this, binding.rb09c, binding.rb09cb, getString(R.string.rb09c))) {
+                return false;
+            }
+
+
+            if (!validatorClass.EmptyRadioButton(this, binding.rb0988, binding.rb0988a, getString(R.string.other))) {
+                return false;
+            }
+
+            if (binding.rb0988a.isChecked()) {
+                if (!validatorClass.EmptyTextBox(this, binding.rb0988x, getString(R.string.other))) {
+                    return false;
+                }
+            }
         }
-
-        if (!binding.rb0899.isChecked() && !validatorClass.EmptyTextBox(this, binding.rb08n, getString(R.string.rb08n))) {
-            return false;
-        }
-
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb09vd, binding.rb09vdb, getString(R.string.rb09vd))) {
-            return false;
-        }
-
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb09i, binding.rb09ib, getString(R.string.rb09i))) {
-            return false;
-        }
-
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb09fa, binding.rb09fab, getString(R.string.rb09fa))) {
-            return false;
-        }
-
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb09m, binding.rb09mb, getString(R.string.rb09m))) {
-            return false;
-        }
-
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb09c, binding.rb09cb, getString(R.string.rb09c))) {
-            return false;
-        }
-
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb0988, binding.rb0988a, getString(R.string.other))) {
-            return false;
-        }
-
 
         if (!validatorClass.EmptyRadioButton(this, binding.rb10, binding.rb1099, getString(R.string.rb10))) {
             return false;
         }
 
+        if (binding.rb10a.isChecked()) {
 
-        if (!validatorClass.EmptyRadioButton(this, binding.rb11, binding.rb1199, getString(R.string.rb11))) {
-            return false;
-        }
+            if (!validatorClass.EmptyRadioButton(this, binding.rb11, binding.rb1199, getString(R.string.rb11))) {
+                return false;
+            }
 
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb12, binding.rb12f, getString(R.string.rb12))) {
-            return false;
+            if (!validatorClass.EmptyRadioButton(this, binding.rb12, binding.rb12f, getString(R.string.rb12))) {
+                return false;
+            }
         }
 
 
@@ -219,18 +253,24 @@ public class SecRBActivity extends Activity {
         }
 
 
-       /* if (binding.rb1699.isChecked() && !validatorClass.EmptyTextBox(this, binding.rb16, getString(R.string.rb16))) {
+        if (!binding.rb1699.isChecked() && !validatorClass.EmptyTextBox(this, binding.rb16, getString(R.string.rb16))) {
             return false;
         }
-        */
+
+
+        if (!binding.rb1699.isChecked() && !validatorClass.RangeTextBox(this, binding.rb16, 1.00, 7.00, getString(R.string.rb16), "KG (weight)")) {
+            return false;
+        }
+
 
         if (!validatorClass.EmptyRadioButton(this, binding.rb17, binding.rb17b, getString(R.string.rb17))) {
             return false;
         }
 
-
-        if (!validatorClass.EmptyRadioButton(this, binding.rb18, binding.rb1888, binding.rb1888x, getString(R.string.other))) {
-            return false;
+        if (binding.rb17a.isChecked()) {
+            if (!validatorClass.EmptyCheckBox(this, binding.fldGrprb18, binding.rb1888, binding.rb1888x, getString(R.string.other))) {
+                return false;
+            }
         }
 
 
@@ -238,7 +278,195 @@ public class SecRBActivity extends Activity {
     }
 
 
+    public void setupViews() {
 
+        binding.rb03.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (binding.rb03b.isChecked()) {
+                    binding.fldGrprb03n.setVisibility(View.GONE);
+                    binding.rb03n.setText(null);
+                } else {
+                    binding.fldGrprb03n.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        binding.rb04.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (binding.rb04b.isChecked()) {
+                    binding.fldGrprb04n.setVisibility(View.GONE);
+                    binding.rb04n.setText(null);
+                } else {
+                    binding.fldGrprb04n.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        binding.rb06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (binding.rb06b.isChecked()) {
+
+                    binding.rb07m.setText(null);
+                    binding.rb0799.setChecked(false);
+                    binding.rb08n.setText(null);
+                    binding.rb0899.setChecked(false);
+                    binding.rb09vd.clearCheck();
+                    binding.rb09i.clearCheck();
+                    binding.rb09fa.clearCheck();
+                    binding.rb09m.clearCheck();
+                    binding.rb09c.clearCheck();
+                    binding.rb0988.clearCheck();
+                    binding.rb0988x.setText(null);
+
+                    binding.fldGrprb07m.setVisibility(View.GONE);
+                } else {
+                    binding.fldGrprb07m.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        binding.rb0799.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (binding.rb0799.isChecked()) {
+                    binding.rb07m.setText(null);
+                    binding.rb07m.setVisibility(View.GONE);
+                } else {
+                    binding.rb07m.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        binding.rb0899.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (binding.rb0899.isChecked()) {
+                    binding.rb08n.setText(null);
+                    binding.rb08n.setVisibility(View.GONE);
+                } else {
+                    binding.rb08n.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        binding.rb0988.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (binding.rb0988a.isChecked()) {
+                    binding.rb0988x.setVisibility(View.VISIBLE);
+                } else {
+                    binding.rb0988x.setText(null);
+                    binding.rb0988x.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        binding.rb10.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (binding.rb10a.isChecked()) {
+                    binding.fldGrprb11.setVisibility(View.VISIBLE);
+                } else {
+                    binding.rb11.clearCheck();
+                    binding.rb12.clearCheck();
+                    binding.fldGrprb11.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+        binding.rb1388.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (binding.rb1388.isChecked()) {
+                    binding.rb1388x.setVisibility(View.VISIBLE);
+                } else {
+                    binding.rb1388x.setText(null);
+                    binding.rb1388x.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+        binding.rb1488.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (binding.rb1488.isChecked()) {
+                    binding.rb1488x.setVisibility(View.VISIBLE);
+                } else {
+                    binding.rb1488x.setText(null);
+                    binding.rb1488x.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+        binding.rb1588.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (binding.rb1588.isChecked()) {
+                    binding.rb1588x.setVisibility(View.VISIBLE);
+                } else {
+                    binding.rb1588x.setText(null);
+                    binding.rb1588x.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+        binding.rb1699.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (binding.rb1699.isChecked()) {
+                    binding.rb16.setText(null);
+                    binding.rb16.setVisibility(View.GONE);
+                } else {
+                    binding.rb16.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        binding.rb17.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (binding.rb17a.isChecked()) {
+                    binding.fldGrprb18.setVisibility(View.VISIBLE);
+                } else {
+                    binding.rb18a.setChecked(false);
+                    binding.rb18b.setChecked(false);
+                    binding.rb18c.setChecked(false);
+                    binding.rb18d.setChecked(false);
+                    binding.rb1888.setChecked(false);
+
+                    binding.fldGrprb18.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+        binding.rb1888.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (binding.rb1888.isChecked()) {
+                    binding.rb1888x.setVisibility(View.VISIBLE);
+                } else {
+                    binding.rb1888x.setText(null);
+                    binding.rb1888x.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+    }
 
 
     /*public boolean ValidateForm() {
