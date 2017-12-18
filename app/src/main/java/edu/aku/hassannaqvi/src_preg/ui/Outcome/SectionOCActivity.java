38ef0a42f0b4269939406de4fee8fa194091d32e
javed.khan <javedak09@gmail.com>
2017-12-18 14:32:27 +0500

    Outcome Preg OC

---
+++ app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/Outcome/SectionOCActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.src_preg.ui.Outcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SectionOCActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_oc);
+    }
+}
