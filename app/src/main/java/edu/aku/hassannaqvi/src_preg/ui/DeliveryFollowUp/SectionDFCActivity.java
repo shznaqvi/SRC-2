19feb1f755a7fb9afddbabf7b13218814db9d53f
ali.azaz <openmind_ali@hotmail.com>
2017-12-21 17:36:01 +0500

    commit

--- app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/DeliveryFollowUp/SectionDFCActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/DeliveryFollowUp/SectionDFCActivity.java
@@ -1,10 +1,13 @@
 package edu.aku.hassannaqvi.src_preg.ui.DeliveryFollowUp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import edu.aku.hassannaqvi.src_preg.R;
+import edu.aku.hassannaqvi.src_preg.core.MainApp;
 import edu.aku.hassannaqvi.src_preg.databinding.ActivitySectionDfcBinding;
 
 public class SectionDFCActivity extends Activity
@@ -19,5 +22,15 @@ public class SectionDFCActivity extends Activity
         bl = DataBindingUtil.setContentView(this, R.layout.activity_section_dfc);
         //setupViews();
         bl.setCallback(this);
+    }
+
+    public void BtnEnd() {
+        finish();
+        Toast.makeText(this, "complete Section", Toast.LENGTH_SHORT).show();
+        MainApp.endActivity(this,this);
+    }
+
+    public void BtnContinue() {
+
     }
 }
