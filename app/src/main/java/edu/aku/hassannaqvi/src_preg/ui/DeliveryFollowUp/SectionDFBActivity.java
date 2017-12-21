c2f6584963728c138bf07b874ac4e6af950d2a58
ali.azaz <openmind_ali@hotmail.com>
2017-12-21 13:01:07 +0500

    section oc complete and section dfd layout done

--- app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/DeliveryFollowUp/SectionDFBActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/DeliveryFollowUp/SectionDFBActivity.java
@@ -1,15 +1,15 @@
-package edu.aku.hassannaqvi.src_preg.ui.ChildFollowUp;
+package edu.aku.hassannaqvi.src_preg.ui.DeliveryFollowUp;
 
 import android.app.Activity;
 import android.os.Bundle;
 
 import edu.aku.hassannaqvi.src_preg.R;
 
-public class SectionCFBActivity extends Activity {
+public class SectionDFBActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section_cfb);
+        setContentView(R.layout.activity_section_dfb);
     }
 }
