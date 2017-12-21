8e2168c750ca4e43bea55e8608817c9df091e322
Gul Sanober <gulsanobermemon@gmail.com>
2017-12-21 17:01:15 +0500

    Section dfc completed

--- app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/DeliveryFollowUp/SectionDFCActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/src_preg/ui/DeliveryFollowUp/SectionDFCActivity.java
@@ -1,16 +1,23 @@
 package edu.aku.hassannaqvi.src_preg.ui.DeliveryFollowUp;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import edu.aku.hassannaqvi.src_preg.R;
+import edu.aku.hassannaqvi.src_preg.databinding.ActivitySectionDfcBinding;
 
 public class SectionDFCActivity extends Activity
 {
+    ActivitySectionDfcBinding bl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section_dfc);
+        //setContentView(R.layout.activity_section_dfc);
+
+        bl = DataBindingUtil.setContentView(this, R.layout.activity_section_dfc);
+        //setupViews();
+        bl.setCallback(this);
     }
 }
